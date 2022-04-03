06d560b250a7095ba58df72cb8762a820768390a
Christoph Rieger <christoph.rieger@MacBook-Pro-2.local>
2022-04-02 21:05:09 -0300

    added middleware to check for authenticated users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test Route
 // @access  Public
-router.get('/', (req,res) => res.send('Auth Route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
